c97243ba966e8bd891a028ba5f585d9a0fe019c3
Acestus <wme.weeks@gmail.com>
2021-01-04 10:38:54 -0600

    New

--- src/aoc/Day02.clj
+++ src/aoc/Day02.clj
@@ -1,1 +1,12 @@
 (ns aoc.Day02)
+; https://adventofcode.com/2020
+(slurp "/home/acestus/IdeaProjects/aoc/src/aoc/02.txt")
+; 1-3 a:abcde
+; 1-3 b:cdefg
+; 2-9 c:ccccccccc
+; How many password are valid according to their policies?
+
+(let [[_           min   max   ch    password]
+      (re-matches #"(\d+)-(\d+) (\w:) (\w+)" "15-16 p: pppppppppplppppp")]
+  min
+  )
--- src/aoc/core.clj
+++ src/aoc/core.clj
@@ -1,29 +1,12 @@
-(ns aoc.core
-  (:require [clojure.string :as s]))
+(ns aoc.core)
+; https://adventofcode.com/2020
+(slurp "/home/acestus/IdeaProjects/aoc/src/aoc/02.txt")
+; 1-3 a:abcde
+; 1-3 b:cdefg
+; 2-9 c:ccccccccc
+; How many password are valid according to their policies?
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-(foo 4)
-
-
-(slurp "/home/acestus/IdeaProjects/aoc/src/aoc/01.txt")
-; Find the two entries that sum to 2020
-; and then multiply those two numbers together.
-(defn read-numbers [filename]
-  (->> filename
-       slurp
-       s/split-lines
-       (map #(Integer/parseInt %))
-       set))
-
-(println
-  (let [numbers (read-numbers "/home/acestus/IdeaProjects/aoc/src/aoc/01.txt")]
-           (for [a numbers
-                 b numbers
-                 :let [c (- 2020 a b)]
-                 :when (< a b c)
-                 :when (contains? numbers c)]
-             [a b c])))
-
+(let [[_           min   max   ch    password]
+      (re-matches #"(\d+)-(\d+) (\w:) (\w+)" "15-16 p: pppppppppplppppp")]
+      min
+      )
